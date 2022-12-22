9f17791b186d62245e99b56c81c9fcb2d4179270
vde-guil <valentin.de.guillebon@gmail.com>
2022-12-22 15:41:17 +0100

    develop: added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 
-
 const startUp = async () => {
+	console.log('starting up...');
 	if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
 	if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
 
